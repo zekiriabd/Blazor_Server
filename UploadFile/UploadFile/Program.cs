9eb4959ab9cb3ee1467fe3cb398273ec5f4f1c16
zekir <zekiriabd@hotmail.com>
2022-12-01 11:00:45 +0100

    Blazor Server UploadFile

---
+++ UploadFile/UploadFile/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using UploadFile.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/index");
+
+app.Run();
