e6e197145071275c6ee816e3817bfb5a2238fc9a
zekir <zekiriabd@hotmail.com>
2023-08-03 11:32:20 +0200

    add Map

---
+++ BlazorMapMarcker/BlazorMapMarcker/Data/MPoint.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorMapMarcker.Data
+{
+    public class MPoint
+    {
+        public int Id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Comment { get; set; }        
+    }
+}
---
+++ DapperBlazorApp/DapperBlazorApp/Data/Entities/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace DapperBlazorApp.Data.Entities
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public int Rank { get; set; }
+    }
+}
